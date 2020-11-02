18a25c82e1909fff41a6f19040d95c8b307a4f1a
mzukic5 <mzukic5@etf.unsa.ba>
2020-11-02 19:40:34 +0100

    prvi komit

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,25 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    private double prvaTacka,drugaTacka;
+    private boolean pripadnostPrveTacke,pripadnostDrugeTacke;
 
+    Interval(){
+        prvaTacka = 0;
+        drugaTacka = 0;
+        pripadnostPrveTacke = false;
+        pripadnostDrugeTacke = false;
+    }
+    Interval(double t1,double t2,boolean b1,boolean b2){
+        if(t1 > t2)throw IllegalArgumentException();
+        prvaTacka = t1;
+        drugaTacka = t2;
+        pripadnostPrveTacke = b1;
+        pripadnostDrugeTacke = b2;
+    }
 
+    @Override
+    boolean isNull(){
+        return prvaTacka == 0 && drugaTacka == 0;
+    }
 }
